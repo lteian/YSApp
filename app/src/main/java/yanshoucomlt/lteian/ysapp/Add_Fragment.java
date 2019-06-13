1c6544ed3dcb96063f2814ebdf2a876bfaea8f0e
lteian <lteian@126.com>
2019-06-14 00:33:37 +0800

    Initial commit

---
+++ app/src/main/java/yanshoucomlt/lteian/ysapp/Add_Fragment.java
@@ -0,0 +1,17 @@
+package yanshoucomlt.lteian.ysapp;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class Add_Fragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View view=inflater.inflate(R.layout.add_fragment,null);
+        return view;
+    }
+}
---
+++ app/src/main/java/yanshoucomlt/lteian/ysapp/User_Fragment.java
@@ -0,0 +1,17 @@
+package yanshoucomlt.lteian.ysapp;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class User_Fragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
+        View view=inflater.inflate(R.layout.user_fragment,null);
+        return view;
+    }
+}
